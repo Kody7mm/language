43acfe94725132550bb613389e3b42abccb8d0ef
Kody Gordon <gordon.km89@gmail.com>
2021-06-04 09:30:42 -0700

    Add basic logic to JS file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#formselector").submit(function (event) {
+    event.preventDefault();
+    const queOne = parseInt($("input:radio[name=queOne]:checked").val());
+    const queTwo = parseInt($("input:radio[name=queTwo]:checked").val());
+    const queThree = parseInt($("input:radio[name=queThree]:checked").val());
+    const queFour = parseInt($("input:radio[name=queFour]:checked").val());
+    const queFive = parseInt($("input:radio[name=queFive]:checked").val());
+    const total = queOne + queTwo + queThree + queFour + queFive;
